26f72d2ab71d9a8cedbbf3c6881858c9c9753c2c
miumida <46196137+miumida@users.noreply.github.com>
2021-12-15 01:36:05 +0900

    Update const.py

--- custom_components/naver_weather/const.py
+++ custom_components/naver_weather/const.py
@@ -22,7 +22,7 @@ DEVICE_UPDATE = "update"
 DEVICE_REG = "register"
 DEVICE_UNREG = "unregister"
 
-SW_VERSION = "2.1.5"
+SW_VERSION = "2.1.6"
 BSE_URL = "https://search.naver.com/search.naver?query={}"
 
 # area
